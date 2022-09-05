5335ef81757971c30a7dc2b0a0f63a76959bb303
Vural Yalçın <vrlylcn@Vurals-MacBook-Pro.local>
2022-09-05 23:42:04 +0300

    mobile header and mobile search toggle development

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,7 @@
 $(function() {
-  console.log("App started..!");
+  $(".js-button-mobile-search").on("click", function(e) {
+    e.preventDefault();
+    $(this).stop().toggleClass("is-active");
+    $(".js-container-search").stop().toggleClass("is-active");
+  });
 });
